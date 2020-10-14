3feb7af4396df2e2f052298ffcb13d526682b47a
Vadim <vadimoleynik039@gamil.com>
2020-10-14 17:18:32 +0300

    work version

--- src/components/App/AppSwitch/AppSwitch.js
+++ src/components/App/AppSwitch/AppSwitch.js
@@ -1,35 +1,34 @@
 import React from 'react'
 import UsersList from '../../UsersList/UsersList'
-import './AppSwitch.css'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
 import AlbumModal from '../../UserPage/Albums/AlbumCard/AlbumModal/AlbumModal'
 import PostList from '../../PostList/PostList'
 import AlbumList from '../../AlbumList/AlbumList'
 import NotFound from '../../NotFound/NotFound'
 
-const AppSwitch = () => {
+const AppSwitch = ({ location }) => {
   return (
     <Switch>
       <Route exact path="/">
-        <PostList url={`https://jsonplaceholder.typicode.com/posts`}/>
+        <PostList url={`https://jsonplaceholder.typicode.com/posts`} />
       </Route>
       <Route path="/albums">
         <Route exact path='/albums/album/:albumId'>
-          <AlbumModal/>
+          <AlbumModal />
         </Route>
-        <AlbumList/>
+        <AlbumList />
       </Route>
       <Route path="/users">
-        <UsersList/>
+        <UsersList />
       </Route>
       <Route exact path='/users/:id/album/:albumId'>
-        <AlbumModal/>
+        <AlbumModal />
       </Route>
       <Route path='*'>
-        <NotFound/>
+        <NotFound />
       </Route>
     </Switch>
   )
 }
 
-export default AppSwitch
+export default withRouter(AppSwitch)
